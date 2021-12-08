4dd15050d7bf4b4e12fd0da6e309ad2af532e541
bdhrs <bodhirasa@gmail.com>
2021-12-08 21:23:11 +0530

    third time lucky

--- dpd_tests.py
+++ dpd_tests.py
@@ -1,333 +1,263 @@
-import csv
-import openpyxl as xl
-import re
-from xlsxwriter.workbook import Workbook
+#!/usr/bin/env python3.10
+# coding: utf-8
+
 import logging
-import excel2json
+import re
+import pandas as pd
+from pandas_ods_reader import read_ods
+from datetime import datetime
+import warnings
+
+warnings.filterwarnings("ignore", 'This pattern has match groups')
+
+#setup time
+now = datetime.now()
+now = now.strftime("%Y/%m/%d %H:%M:%S")
+
 
 #setup logger
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%I:%M:%S')
 
+#convert ods to csv
+from ods_to_csv import convert_dpd_ods_to_csv
+yn = (input(f"convert ods to csv? "))
+if yn == "y":
+	convert_dpd_ods_to_csv()
+if yn == "n":
+	pass
 
-logging.warning(f"reading csv_file")
-csv_file = "/home/bhikkhu/Bodhirasa/Dropbox/Pāli English Dictionary/Pāli English Dictionary.csv"
-df = read_csv (csv_file, sep="\t")
+#import dpd csv
+logging.warning(f"opening dpd.csv as data frame")
+csv_file = "/home/bhikkhu/Bodhirasa/Dropbox/dpd/dpd.csv"
+pali_df = pd.read_csv (csv_file, sep="\t", dtype=str, skip_blank_lines=False)
+pali_df.fillna("", inplace=True)
 
-# # convert csv to excel
+#import tests
+logging.warning(f"opening tests.ods as data frame")
+tests_csv_file = "/home/bhikkhu/Bodhirasa/Dropbox/dpd/dpd_data_tests/tests.csv"
+sheet_index = 1
+tests_df = pd.read_csv (tests_csv_file, sep="\t", dtype=str, skip_blank_lines=False)
+tests_df.fillna("", inplace=True)
 
-# convert = input (f"convert csv to excel (y/n)? ")
-# if convert == "y":
-#     logging.warning(f"converting csv to excel sheet")
-#     csv_file = "/home/bhikkhu/Bodhirasa/Dropbox/Pāli English Dictionary/Pāli English Dictionary-full.csv"
-#     xlsx_file = "/home/bhikkhu/Bodhirasa/Dropbox/Pāli English Dictionary/Pāli English Dictionary-full.xlsx"
-#     workbook = Workbook(xlsx_file)
-#     worksheet = workbook.add_worksheet()
-#     csv_reader = csv.reader(open(csv_file,'rt'),delimiter="\t")
-
-#     for row, data in enumerate(csv_reader):
-#         worksheet.write_row(row, 0, data)
-#     workbook.close()
-    
-# if convert == "n":
-#     pass
-
-# open excel sheet
-logging.warning ("loading excel sheet")
-wb = xl.load_workbook("Pāli English Dictionary-full.xlsx")
-sheet = wb["Sheet1"]
-pali_last_row = sheet.max_row + 1
-
-#convert excel to json
-logging.warning ("coverting excel sheet to json")
-excel2json.convert_from_file('json_test.xls')
-
-
-#open txt file
-logging.warning ("opening txt file")
+#open test_results.txt
+logging.warning ("opening test_results.txt")
 txt_file = open ("test_results.txt", 'w', encoding= "'utf-8")
 
+test_column_count = tests_df.shape[0]
 
+#define variables
+logging.warning ("defining variables")
 
-def search_for(
-	test_name,
-	search1_column,
-	search1_string,
-	search2_column,
-	search2_string,
-	search3_column,
-	search3_string,
-	search4_column,
-	search4_string,
-	print2_column,
-	print3_column,
-	print4_column,
-	exceptions,
-	iterations):
-
-	logging.warning (f"test for {test_name}")
-
+#print to text file
+# logging.warning (f"{row}. {search_name}")
+with open("test_results.txt", 'a') as txt_file:
 	line_break = "~" * 40
-
+	txt_file.write (f"DPD tests {now}\n")
 	txt_file.write (f"{line_break}\n")
-	txt_file.write (f"{test_name}\n")
+	txt_file.write (f"Notez in Anki\n")
+	txt_file.write (f"Notez in Google Sheets\n")
+	txt_file.write (f"Notez on page\n")
+
+for row in range (0, test_column_count):
+	search_name = (tests_df.iloc[row, 0])
+
+	if search_name == (""):
+		continue
+	elif re.findall("^\!", search_name):
+		continue
+
+	search_column1 = (tests_df.iloc[row, 1])
+	search_sign1 = (tests_df.iloc[row, 2])
+	search_string1 = (tests_df.iloc[row, 3])
+
+	search_column2 = (tests_df.iloc[row, 4])
+	search_sign2 = (tests_df.iloc[row, 5])
+	search_string2 = (tests_df.iloc[row, 6])
+
+	search_column3 = (tests_df.iloc[row, 7])
+	search_sign3 = (tests_df.iloc[row, 8])
+	search_string3 = (tests_df.iloc[row, 9])
+
+	search_column4 = (tests_df.iloc[row, 10])
+	search_sign4 = (tests_df.iloc[row, 11])
+	search_string4 = (tests_df.iloc[row, 12])
+
+	search_column5 = (tests_df.iloc[row, 13])
+	search_sign5 = (tests_df.iloc[row, 14])
+	search_string5 = (tests_df.iloc[row, 15])
+
+	search_column6 = (tests_df.iloc[row, 16])
+	search_sign6 = (tests_df.iloc[row, 17])
+	search_string6 = (tests_df.iloc[row, 18])
+
+	print_column1 = (tests_df.iloc[row, 19])
+	print_column2 = (tests_df.iloc[row, 20])
+	print_column3 = (tests_df.iloc[row, 21])
+
+	exceptions = (tests_df.iloc[row, 22])
+	iterations = int(tests_df.iloc[row, 23])
+
+	if exceptions == "":
+		test_exceptions = pali_df["Pāli1"].str.contains(".+")
+	if exceptions != "":
+		test_exceptions = pali_df["Pāli1"].str.contains(exceptions) == False
+	
+	#search1
+	if search_sign1 == "equals":
+		test1 = pali_df[search_column1] == (search_string1)
+	elif search_sign1 == "does not equal":
+		test1 = pali_df[search_column1] != (search_string1)
+	elif search_sign1 == "contains":
+		test1 = pali_df[search_column1].str.contains(search_string1)
+	elif search_sign1 == "does not contain":
+		test1 = pali_df[search_column1].str.contains(search_string1) == False
+	elif search_sign1 == "contains word":
+		test1 = pali_df[search_column1].str.contains(fr"\b{search_string1}\b")
+	elif search_sign1 == "does not contain word":
+		test1 = pali_df[search_column1].str.contains(fr"\b{search_string1}\b") == False
+	elif search_sign1 == "is empty":
+		test1 = pali_df[search_column1] == ("")
+	elif search_sign1 == "is not empty":
+		test1 = pali_df[search_column1].str.contains(".+") == True
+	elif search_sign1 == "":
+		test1 = pali_df["Pāli1"].str.contains(".+")
+	else:
+		print(f"search1 error")
+
+	#search2
+	if search_sign2 == "equals":
+		test2 = pali_df[search_column2] == (search_string2)
+	elif search_sign2 == "does not equal":
+		test2 = pali_df[search_column2] != (search_string2)
+	elif search_sign2 == "contains":
+		test2 = pali_df[search_column2].str.contains(search_string2)
+	elif search_sign2 == "does not contain":
+		test2 = pali_df[search_column2].str.contains(search_string2) == False
+	elif search_sign2 == "contains word":
+		test2 = pali_df[search_column2].str.contains(fr"\b{search_string2}\b")
+	elif search_sign2 == "does not contain word":
+		test2 = pali_df[search_column2].str.contains(fr"\b{search_string2}\b") == False
+	elif search_sign2 == "is empty":
+		test2 = pali_df[search_column2] == ("")
+	elif search_sign2 == "is not empty":
+		test2 = pali_df[search_column2].str.contains(".+") == True
+	elif search_sign2 == "":
+		test2 = pali_df["Pāli2"].str.contains(".+")
+	else:
+		print("search2 error")
+
+	#search3
+	if search_sign3 == "equals":
+		test3 = pali_df[search_column3] == (search_string3)
+	elif search_sign3 == "does not equal":
+		test3 = pali_df[search_column3] != (search_string3)
+	elif search_sign3 == "contains":
+		test3 = pali_df[search_column3].str.contains(search_string3)
+	elif search_sign3 == "does not contain":
+		test3 = pali_df[search_column3].str.contains(search_string3) == False
+	elif search_sign3 == "contains word":
+		test3 = pali_df[search_column3].str.contains(fr"\b{search_string3}\b")
+	elif search_sign3 == "does not contain word":
+		test3 = pali_df[search_column3].str.contains(fr"\b{search_string3}\b") == False
+	elif search_sign3 == "is empty":
+		test3 = pali_df[search_column3] == ("")
+	elif search_sign3 == "is not empty":
+		test3 = pali_df[search_column3].str.contains(".+") == True
+	elif search_sign3 == "":
+		test3 = pali_df["Pāli1"].str.contains(".+")
+	else:
+		print("search3 error")
+
+	#search4
+	if search_sign4 == "equals":
+		test4 = pali_df[search_column4] == (search_string4)
+	elif search_sign4 == "does not equal":
+		test4 = pali_df[search_column4] != (search_string4)
+	elif search_sign4 == "contains":
+		test4 = pali_df[search_column4].str.contains(search_string4)
+	elif search_sign4 == "does not contain":
+		test4 = pali_df[search_column4].str.contains(search_string4) == False
+	elif search_sign4 == "contains word":
+		test4 = pali_df[search_column4].str.contains(fr"\b{search_string4}\b")
+	elif search_sign4 == "does not contain word":
+		test4 = pali_df[search_column4].str.contains(fr"\b{search_string4}\b") == False
+	elif search_sign4 == "is empty":
+		test4 = pali_df[search_column4] == ("")
+	elif search_sign4 == "is not empty":
+		test4 = pali_df[search_column4].str.contains(".+") == True
+	elif search_sign4 == "":
+		test4 = pali_df["Pāli1"].str.contains(".+")
+	else:
+		print("search4 error")
+
+	#search5
+	if search_sign5 == "equals":
+		test5 = pali_df[search_column5] == (search_string5)
+	elif search_sign5 == "does not equal":
+		test5 = pali_df[search_column5] != (search_string5)
+	elif search_sign5 == "contains":
+		test5 = pali_df[search_column5].str.contains(search_string5)
+	elif search_sign5 == "does not contain":
+		test5 = pali_df[search_column5].str.contains(search_string5) == False
+	elif search_sign5 == "contains word":
+		test5 = pali_df[search_column5].str.contains(fr"\b{search_string5}\b")
+	elif search_sign5 == "does not contain word":
+		test5 = pali_df[search_column5].str.contains(fr"\b{search_string5}\b") == False
+	elif search_sign5 == "is empty":
+		test5 = pali_df[search_column5] == ("")
+	elif search_sign5 == "is not empty":
+		test5 = pali_df[search_column5].str.contains(".+") == True
+	elif search_sign5 == "":
+		test5 = pali_df["Pāli1"].str.contains(".+")
+	else:
+		print("search5 error")
+
+	#search6
+	if search_sign6 == "equals":
+		test6 = pali_df[search_column6] == (search_string6)
+	elif search_sign6 == "does not equal":
+		test6 = pali_df[search_column6] != (search_string6)
+	elif search_sign6 == "contains":
+		test6 = pali_df[search_column6].str.contains(search_string6)
+	elif search_sign6 == "does not contain":
+		test6 = pali_df[search_column6].str.contains(search_string6) == False
+	elif search_sign6 == "contains word":
+		test6 = pali_df[search_column6].str.contains(fr"\b{search_string6}\b")
+	elif search_sign6 == "does not contain word":
+		test6 = pali_df[search_column6].str.contains(fr"\b{search_string6}\b") == False
+	elif search_sign6 == "is empty":
+		test6 = pali_df[search_column6] == ("")
+	elif search_sign6 == "is not empty":
+		test6 = pali_df[search_column6].str.contains(".+") == True
+	elif search_sign6 == "":
+		test6 = pali_df["Pāli1"].str.contains(".+")
+	else:
+		print("search6 error")
+
+	filter = test_exceptions & test1 & test2 & test3 & test4 & test5 & test6
+
+	filtered_df = pali_df.loc[filter, [print_column1, print_column2, print_column3]]
+
+	column_count2 = filtered_df.shape[0]
+	filtered_df = filtered_df.head(iterations)
+	column_count1 = filtered_df.shape[0]
+
+	#print to text file
+	logging.warning (f"{row}. {search_name}")
+
+	if column_count1> 0:
+		with open("test_results.txt", 'a') as txt_file:
+			txt_file.write (f"{line_break}\n")
+			txt_file.write (f"{row}. {search_name} ({column_count1} of {column_count2})\n")
+			txt_file.write (f"{line_break}\n")
+			filtered_df.to_csv(txt_file, header=False, index=False, sep="\t")
+			# txt_file.write (f"\n")
+
+with open("test_results.txt", 'a') as txt_file:
+	headings = list(pali_df.columns.values)
+	txt_file.write (f"\n")
 	txt_file.write (f"{line_break}\n")
+	txt_file.write (f"{headings}")
 
-	row_number = 2
-	count = 0
-
-	for row in range(2, pali_last_row):
-		while count < int(iterations):
-			search1_cell = sheet.cell(row_number, int(search1_column))
-			search1_cell_value = str(search1_cell.value)
-			search1 = re.search(search1_string, search1_cell_value)
-
-			if search2_string != "":
-				search2_cell = sheet.cell(row_number, int(search2_column))
-				search2_cell_value = str(search2_cell.value)
-				search2 = re.search(search2_string, search2_cell_value)
-			else:
-				search2 = 0
-
-			if search3_string != "":
-				search3_cell = sheet.cell(row_number, int(search3_column))
-				search3_cell_value = str(search3_cell.value)
-				search3 = re.search(search3_string, search3_cell_value)
-			else:
-				search3 = 0
-
-			if search4_string != "":
-				search4_cell = sheet.cell(row_number, int(search4_column))
-				search4_cell_value = str(search4_cell.value)
-				search4 = re.search(search4_string, search4_cell_value)
-			else:
-				search4 = 0
-			
-			headword_cell = sheet.cell(row_number, 1)
-			headword_cell_value = str(headword_cell.value)
-
-			if print2_column != "":
-				print2_cell = sheet.cell(row_number, int(print2_column))
-				print2_cell_value = str(print2_cell.value)
-			else:
-				print2_cell_value = ""
-
-			if print3_column != "":
-				print3_cell = sheet.cell(row_number, int(print3_column))
-				print3_cell_value = str(print3_cell.value)
-			else:
-				print3_cell_value = ""
-
-			if print4_column != "":
-				print4_cell = sheet.cell(row_number, int(print4_column))
-				print4_cell_value = str(print4_cell.value)
-			else:
-				print4_cell_value = ""
-
-			print_seperator = " || "
-			
-				
-			if (test_name != None
-			and search1 != None 
-			and search2 != None
-			and search3 != None
-			and search4 != None
-			and headword_cell_value not in exceptions
-			):
-				txt_file.write(f"{headword_cell_value}{print_seperator}{print2_cell_value}{print_seperator}{print3_cell_value}{print_seperator}{print4_cell_value}\n")
-				count += 1
-			row_number += 1
-
-
-search_for(
-	test_name = "anā aṇā is not abstr",
-	search1_column = "29",
-	search1_string = "(anā|aṇā)",
-	search2_column = "5",
-	search2_string = "^((?!abstr).)*$",
-	search3_column = "",
-	search3_string = "",
-	search4_column = "",
-	search4_string = "",
-	print2_column = "11",
-	print3_column = "50",
-	print4_column = "",
-	exceptions = ["adhikuṭṭanā"],
-	iterations = "3"
-	)
-
-search_for(
-	test_name = "ti is not abstr",
-	search1_column = "4", 
-	search1_string = "fem",
-	search2_column = "29",
-	search2_string = "^ti$",
-	search3_column = "5",
-	search3_string = "(?s)^((?!abstr).)*$",
-	search4_column = "",
-	search4_string = "",
-	print2_column = "4", 
-	print3_column = "11",
-	print4_column = "50",
-	exceptions = [""],
-	iterations = "3"
-	)
-
-search_for(
-	test_name = "ā fem is not abstr",
-	search1_column = "4", 
-	search1_string = "fem",
-	search2_column = "29",
-	search2_string = "^ā$",
-	search3_column = "5",
-	search3_string = "(?s)^((?!abstr).)*$",
-	search4_column = "",
-	search4_string = "",
-	print2_column = "4", 
-	print3_column = "11",
-	print4_column = "50",
-	exceptions = ["akkharā", "ajā", "ativisā", "sutā"],
-	iterations = "3"
-	)
-
-search_for(
-	test_name = "tā tta tha is not abst",
-	search1_column = "29", 
-	search1_string = "(tā|tta|tha)$",
-	search2_column = "4",
-	search2_string = "(f|nt)",
-	search3_column = "5",
-	search3_string = "(?s)^((?!abstr).)*$",
-	search4_column = "",
-	search4_string = "",
-	print2_column = "4", 
-	print3_column = "11",
-	print4_column = "50",
-	exceptions = [""],
-	iterations = "3"
-	)
-
-search_for(
-	test_name = "ana|aṇa|anā|aṇā is not act",
-	search1_column = "4", 
-	search1_string = "nt",
-	search2_column = "29",
-	search2_string = "ana|aṇa|anā|aṇā",
-	search3_column = "5",
-	search3_string = "(?s)^((?!act).)*$",
-	search4_column = "",
-	search4_string = "",
-	print2_column = "4", 
-	print3_column = "11",
-	print4_column = "50",
-	exceptions = [""],
-	iterations = "3"
-	)
-
-search_for(
-	test_name = "case no constr line 2",
-	search1_column = "5", 
-	search1_string = " (nom|acc|instr|dat|abl|gen|loc|voc)",
-	search2_column = "27",
-	search2_string = "(?s)^((?!<br).)*$",
-	search3_column = "",
-	search3_string = "",
-	search4_column = "",
-	search4_string = "",
-	print2_column = "4", 
-	print3_column = "11",
-	print4_column = "50",
-	exceptions = ["aghato"],
-	iterations = "5"
-	)
-
-# search_for(
-# 	test_name = "",
-# 	search1_column = "", 
-# 	search1_string = "",
-# 	search2_column = "",
-# 	search2_string = "",
-# 	search3_column = "",
-# 	search3_string = "",
-# 	search4_column = "",
-# 	search4_string = "",
-# 	print2_column = "4", 
-# 	print3_column = "11",
-# 	print4_column = "50",
-# 	exceptions = [""],
-# 	iterations = "5"
-# 	)
-
-# search_for(
-# 	test_name = "",
-# 	search1_column = "", 
-# 	search1_string = "",
-# 	search2_column = "",
-# 	search2_string = "",
-# 	search3_column = "",
-# 	search3_string = "",
-# 	search4_column = "",
-# 	search4_string = "",
-# 	print2_column = "4", 
-# 	print3_column = "11",
-# 	print4_column = "50",
-# 	exceptions = [""],
-# 	iterations = "5"
-# 	)
-
-logging.warning (f"saved to {txt_file.name}")
-logging.warning ("fin")
-
-# 1	Pāli1
-# 2	Pāli2
-# 3	Fin
-# 4	POS
-# 5	Grammar
-# 6	Derived from
-# 7	Neg
-# 8	Verb
-# 9	Trans
-# 10	Case
-# 11	Meaning IN CONTEXT
-# 12	Literal Meaning
-# 13	Non IA
-# 14	Sanskrit
-# 15	Sk Root
-# 16	Sk Root Mn
-# 17	Cl
-# 18	Pāli Root
-# 19	Root In Comps
-# 20	V
-# 21	Grp
-# 22	Sgn
-# 23	Root Meaning
-# 24	Base
-# 25	Family
-# 26	Family2
-# 27	Construction
-# 28	Derivative
-# 29	Suffix
-# 30	Phonetic Changes
-# 31	Compound
-# 32	Compound Construction
-# 33	Non-Root In Comps
-# 34	Source1
-# 35	Sutta1
-# 36	Example1
-# 37	Source 2
-# 38	Sutta2
-# 39	Example 2
-# 40	Antonyms
-# 41	Synonyms – different word
-# 42	Variant – same constr or diff reading
-# 43	Commentary
-# 44	Notes
-# 45	Cognate
-# 46	Category
-# 47	Link
-# 48	Stem
-# 49	Pattern
-# 50	Buddhadatta
-# 51	22
-# 52	Pāli1 ≠ const
-# 53	test dupl
-# 54	Metadata
+# report errors on wrong fields
+# make a dataframe of the list names and count as you go along 
